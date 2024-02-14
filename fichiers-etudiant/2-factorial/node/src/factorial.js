7425c2c8ffa3ca8770bc3abcfbe0f22c93472f12
Hugo <hugo.follmi@etu.univ-grenoble-alpes.fr>
2024-02-14 15:46:54 +0100

    first commit

---
+++ fichiers-etudiant/2-factorial/node/src/factorial.js
@@ -0,0 +1,10 @@
+/**
+ * Return the factorial of i.
+ * @param {number} i - an integer number.
+ * @return {number} the factorial of i.
+ */
+let factorial = function (i) {
+  // À compléter.
+};
+
+module.exports = factorial;
