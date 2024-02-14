1f6b6b4e13f54ed42a0e9c8484913c57eca671f7
Hugo <hugo.follmi@etu.univ-grenoble-alpes.fr>
2024-02-14 16:44:08 +0100

    exo 1

--- fichiers-etudiant/1-money/node/test/test-money.js
+++ fichiers-etudiant/1-money/node/test/test-money.js
@@ -1,12 +1,18 @@
 const Money = require("../src/money.js");
 const chai = require("chai");
 const assert = require("chai").assert;
+const should = require("chai").should();
 
 describe("Money", function () {
   describe("#add()", function () {
+    let m1;
+
+    beforeEach(function () {
+      m1 = new Money(10.0, "EUR");
+    });
+
     it("should correctly add two moneys with the same currency", function () {
-      let m1 = new Money(10.0, "EUR"),
-        m2 = new Money(20.0, "EUR");
+      m2 = new Money(20.0, "EUR");
 
       m1.add(m2); // On additionne m2 à m1. m1 est modifié
 
@@ -21,25 +27,22 @@ describe("Money", function () {
     });
 
     it("should correctly add two moneys with different currencies", function () {
-      let m1 = new Money(10.0, "EUR"),
-        m2 = new Money(20.0, "USD");
+      m2 = new Money(20.0, "USD");
 
       m1.add(m2); // On additionne m2 à m1. m1 est modifié
 
       let newAmount = m1.amount, // On récupère la valeur
         oracle = 20.0; // On souhaite comparer par rapport à la valeur théorique
 
-      assert.equal(newAmount, oracle); // On écrit une assertion
+      newAmount.should.equal(oracle);
     });
 
     it("should throw an exception when the currency is neither EUR nor USD", function () {
-      let m1 = new Money(10.0, "EUR"),
-        m2 = new Money(20.0, "BZR"); // BZR : Réal brésilien
+      m2 = new Money(20.0, "BZR"); // BZR : Réal brésilien
 
-      assert.throws(function () {
-        // On capture l'exception
-        m1.add(m2);
-      });
+      chai.expect(function () {
+          m1.add(m2);
+        }).to.throw("Unknown currency");
     });
   });
 });
