72b3c328c9b667ad07bda8e8036b00c4ffc54e2f
Hugo <hugo.follmi@etu.univ-grenoble-alpes.fr>
2024-02-16 13:34:13 +0100

    init tp 2

--- fichiers-etudiant/3-levenshtein/test/test-levenshtein.js
+++ fichiers-etudiant/3-levenshtein/test/test-levenshtein.js
@@ -17,4 +17,18 @@ describe('#levenshtein()', function () {
   it("La distance de levenshtein entre 'niche' et '' est de 5", function () {
     assert.equal(levenshtein('niche', '').distance, 5);
   });
+
+  it("La distance de levenshtein entre 'niche' et 'niche' est de 0", function () {
+    assert.equal(levenshtein('niche', 'niche').distance, 0);
+  });
+
+  it("La distance de levenshtein entre '' et 'niche' est de 5", function () {
+    assert.equal(levenshtein('', 'niche').distance, 5);
+  });
+
+  it("La distance de levenshtein entre null et 3 doit lancer une exception", function () {
+    assert.throws(function () {
+      levenshtein(null, 3);
+    });
+  });
 });
